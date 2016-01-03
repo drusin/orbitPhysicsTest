b3d4ab77da1189ca484c9952a3dae5bfbbe9b325
BuildTools <dawidrusin90@gmail.com>
2016-01-03 16:57:49 +0100

    Using ellipses for planets and goals instead of rectangles

--- core/src/dawid/orbitprototype/util/LevelLoader.java
+++ core/src/dawid/orbitprototype/util/LevelLoader.java
@@ -1,9 +1,11 @@
 package dawid.orbitprototype.util;
 
 import com.badlogic.ashley.core.Engine;
+import com.badlogic.gdx.maps.objects.EllipseMapObject;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.math.Ellipse;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
@@ -29,19 +31,19 @@ public class LevelLoader {
 			engine.addEntity(new SpawnerEntity(new Vector2(x, y), new Vector2(scaleDown(vx), scaleDown(vy))));
 		}
 
-		for (RectangleMapObject object : map.getLayers().get(TiledConstants.PLANET_LAYER).getObjects().getByType(RectangleMapObject.class)) {
-			Rectangle rectangle = object.getRectangle();
-			float x = rectangle.getX() + rectangle.getWidth() / 2;
-			float y = rectangle.getY() + rectangle.getHeight() / 2;
-			float radius = (rectangle.getWidth() + rectangle.getHeight()) / 4;
+		for (EllipseMapObject object : map.getLayers().get(TiledConstants.PLANET_LAYER).getObjects().getByType(EllipseMapObject.class)) {
+			Ellipse ellipse = object.getEllipse();
+			float x = ellipse.x + ellipse.width / 2;
+			float y = ellipse.y + ellipse.height / 2;
+			float radius = (ellipse.width + ellipse.height) / 4;
 			engine.addEntity(new PlanetEntity(world, x, y, radius));
 		}
 
-		for (RectangleMapObject object : map.getLayers().get(TiledConstants.GOAL_LAYER).getObjects().getByType(RectangleMapObject.class)) {
-			Rectangle rectangle = object.getRectangle();
-			float x = rectangle.getX() + rectangle.getWidth() / 2;
-			float y = rectangle.getY() + rectangle.getHeight() / 2;
-			float radius = (rectangle.getWidth() + rectangle.getHeight()) / 4;
+		for (EllipseMapObject object : map.getLayers().get(TiledConstants.GOAL_LAYER).getObjects().getByType(EllipseMapObject.class)) {
+			Ellipse ellipse = object.getEllipse();
+			float x = ellipse.x + ellipse.width / 2;
+			float y = ellipse.y + ellipse.height / 2;
+			float radius = (ellipse.width + ellipse.height) / 4;
 			engine.addEntity(new GoalEntity(world, x, y, radius));
 		}
 	}
